1a75e28ac4ac1066ceabe022e1034f73075092c9
vernonroque <vroque88@gmail.com>
2024-07-24 12:17:06 -0500

    Finished all CRUD functionalities

--- django_restaurant/restaurant_app/urls.py
+++ django_restaurant/restaurant_app/urls.py
@@ -3,12 +3,17 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name="home"),
-  path('new_recipe/',views.new_recipe,name="new_recipe"),
   path('recipe_create/', views.RecipeCreate.as_view() ,name="recipecreate"),
-  path('new_ingredient/', views.new_ingredient, name="new_ingredient"),
-  path('customer_purchase/', views.customer_purchase, name="customer_purchase"),
   path('recipe_list/',views.RecipeList.as_view(), name="recipelist"),
   path('recipe_update/<pk>',views.RecipeUpdate.as_view(), name="recipeupdate"),
-  path('recipe_delete/<pk>',views.RecipeDelete.as_view(), name="recipedelete")
+  path('recipe_delete/<pk>',views.RecipeDelete.as_view(), name="recipedelete"),
+  path('ingredient_list/',views.IngredientList.as_view(), name="ingredientlist"),
+  path('ingredient_create/', views.IngredientCreate.as_view() ,name="ingredientcreate"),
+  path('ingredient_update/<pk>',views.IngredientUpdate.as_view(), name="ingredientupdate"),
+  path('ingredient_delete/<pk>',views.IngredientDelete.as_view(), name="ingredientdelete"),
+  path('customer_purchase_list/',views.PurchaseList.as_view(), name="customerpurchaselist"),
+  path('customer_purchase_create/', views.CustomerPurchaseCreate.as_view() ,name="customerpurchasecreate"),
+  path('customer_purchase_update/<pk>',views.CustomerPurchaseUpdate.as_view(), name="customerpurchaseupdate"),
+  path('customer_purchase_delete/<pk>',views.CustomerPurchaseDelete.as_view(), name="customerpurchasedelete"),
 
 ]
--- django_restaurant/restaurant_app/views.py
+++ django_restaurant/restaurant_app/views.py
@@ -8,26 +8,15 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .forms import RecipeCreateForm
 
-recipes = [{
-    'recipe_name':'grilled cheese', 
-    'cost':'4',
-    'ingredients':['bread','cheese']}]
+# recipes = [{
+#     'recipe_name':'grilled cheese', 
+#     'cost':'4',
+#     'ingredients':['bread','cheese']}]
 
 # Create your views here.
 def home(request):
-    context = {"name": "Jenkins","recipes":recipes}
-    return render(request,"restaurant_app/home.html", context)
-
-# def new_recipe(request):
-#     context = {"recipes":recipes}
-#     return render(request,"restaurant_app/new_recipe.html",context)
-
-def new_ingredient(request):
-    context = {"recipes":recipes}
-    return render(request,"restaurant_app/new_ingredient.html",context)
-def customer_purchase(request):
-    context = {"recipes":recipes}
-    return render(request,"restaurant_app/customer_purchase.html",context)
+    #context = {"name": "Jenkins","recipes":recipes}
+    return render(request,"restaurant_app/home.html")
 
 class RecipeList(ListView):
     model = Recipe
@@ -51,7 +40,51 @@ class RecipeDelete(DeleteView):
     template_name = 'restaurant_app/recipe_delete_form.html'
     success_url = reverse_lazy('recipelist')
 
-#Another way to create a form with more customization
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = 'restaurant_app/ingredient_list.html'
+    context_object_name = 'ingredients'
+
+class IngredientCreate(CreateView):
+    model = Ingredient
+    template_name = "restaurant_app/ingredient_create_form.html"
+    fields = ["name", "price_per_unit", "items_available"]
+    success_url = reverse_lazy('ingredientlist')
+
+class IngredientUpdate(UpdateView):
+    model = Ingredient
+    template_name = 'restaurant_app/ingredient_update_form.html'
+    fields = ["name", "price_per_unit", "items_available"]
+    success_url = reverse_lazy('ingredientlist')
+
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = 'restaurant_app/ingredient_delete_form.html'
+    success_url = reverse_lazy('ingredientlist')
+
+class PurchaseList(ListView):
+    model = Purchase
+    template_name = 'restaurant_app/customer_purchase_list.html'
+    context_object_name = 'purchases'
+
+class CustomerPurchaseCreate(CreateView):
+    model = Purchase
+    template_name = "restaurant_app/customer_purchase_create_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy('customerpurchaselist')
+
+class CustomerPurchaseUpdate(UpdateView):
+    model = Purchase
+    template_name = 'restaurant_app/customer_purchase_update_form.html'
+    fields = "__all__"
+    success_url = reverse_lazy('customerpurchaselist')
+
+class CustomerPurchaseDelete(DeleteView):
+    model = Purchase
+    template_name = 'restaurant_app/customer_purchase_delete_form.html'
+    success_url = reverse_lazy('customerpurchaselist')
+
+#Another way to create a form with more customization. This currently not being used
 def new_recipe(request):
     if request.method == "POST":
         form = RecipeCreateForm(request.POST)
